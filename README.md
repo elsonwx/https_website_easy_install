dc584d3988ef06f88564c865efd2c1656bd4fa89
wangxiang <xiang_wang@pinssible.com>
2017-06-13 18:44:27 +0800

    init this repo

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # https_website_easy_install
+one command to generate https website on nginx
+
+## Usage
+
+- install nginx on linux
+
+  - ubuntu/debian
+
+    ````
+    $ sudo apt-get install nginx
+    ````
+
+  - centos/redhat
+
+    ```
+    $ sudo yum install nginx
+    ```
+
+    ​
+
+- download this script
+
+  ``` 
+  $wget 
+  ```
+
+  ​
+
+- generate https website on your linux server
+
+  ```
+  $ sudo bash https_website_easy_install.bash
+  ```
+
+  ​
---
+++ https_website_easy_install.bash
@@ -0,0 +1,131 @@
+#!/bin/bash
+check_ip() {
+    IP_REGEX="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+    printf %s "$1" | tr -d '\n' | grep -Eq "$IP_REGEX"
+}
+PUBLIC_IP=$(dig @resolver1.opendns.com -t A -4 myip.opendns.com +short)
+check_ip "$PUBLIC_IP" || PUBLIC_IP=$(wget -t 3 -T 15 -qO- http://ipv4.icanhazip.com)
+check_ip "$PUBLIC_IP" || exiterr "Cannot find your server ip address"
+
+echo "your server ip is:${PUBLIC_IP}"
+echo "please input your website domain which has been resolved to $PUBLIC_IP"
+echo "if you want to binding more than one domain,you can split them using space"
+read -p "> " web_domains
+domain_length=0
+sign_domain_str=''
+web_first_domain=$(echo $web_domains|tr -s [:blank:]|cut -d ' ' -f 1)
+nginx_web_config_file=$web_first_domain".conf"
+for web_domain in ${web_domains[@]}
+do
+    sign_domain_str=$sign_domain_str"DNS:"$web_domain","
+    domain_length=$(($domain_length+1))
+done
+sign_domain_str=${sign_domain_str:0:${#sign_domain_str}-1}
+echo "please input your website complete absolute path"
+read -p "> " web_dir
+if [[ ! "$web_dir" == /* ]]; then
+	web_dir=$(pwd)"/"$web_dir
+fi
+echo $web_dir
+mkdir -p ${web_dir}"/certificate/challenges"
+chmod -R 755 $web_dir
+echo "please input the nginx config dir"
+echo "you can carrige return if it's default /etc/nginx"
+read -p "> " nginx_config_dir
+if [[ -z "$nginx_config_dir" ]]; then
+    nginx_config_dir=/etc/nginx
+fi
+echo -e "\n"
+cat << EOF
+please confirm the information is correct
+web_dir:$web_dir
+domain:$web_domains
+nginx_config_dir:$nginx_config_dir
+1):confirm
+2):quit
+EOF
+read -p "> " confirm
+if [[  $confirm -eq 2 ]]; then
+    exit 0
+fi
+cd $web_dir"/certificate"
+# Create a Let's Encrypt account private key
+openssl genrsa 4096 > account.key
+# generate a domain private key
+openssl genrsa 4096 > domain.key
+if [[ $domain_length -gt 1 ]]; then
+    openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=$sign_domain_str")) > domain.csr
+else
+    openssl req -new -sha256 -key domain.key -subj "/CN=$web_domains" > domain.csr
+fi
+cat > $nginx_config_dir"/conf.d/"$nginx_web_config_file <<EOF
+server {
+    listen 80;
+    server_name $web_domains;
+    location /.well-known/acme-challenge/ {
+        alias $web_dir/certificate/challenges/;
+        try_files \$uri =404;
+    }
+}
+EOF
+service  nginx restart
+wget https://raw.githubusercontent.com/diafygi/acme-tiny/master/acme_tiny.py
+python acme_tiny.py --account-key ./account.key --csr ./domain.csr --acme-dir $web_dir/certificate/challenges > ./signed.crt
+#NOTE: For nginx, you need to append the Let's Encrypt intermediate cert to your cert
+wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate.pem
+cat signed.crt intermediate.pem > chained.pem
+cat > $nginx_config_dir"/conf.d/"$nginx_web_config_file <<EOF
+server {
+    listen 80;
+    server_name $web_domains;
+    rewrite ^(.*) https://\$host\$1 permanent;
+}
+server {
+        listen 443;
+        server_name $web_domains;
+        root $web_dir;
+        index index.html index.htm index.php;
+        ssl on;
+        ssl_certificate $web_dir/certificate/chained.pem;
+        ssl_certificate_key $web_dir/certificate/domain.key;
+        ssl_protocols SSLv3 TLSv1 TLSv1.1 TLSv1.2;
+        ssl_ciphers ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA;
+        ssl_session_cache shared:SSL:50m;
+        ssl_prefer_server_ciphers on;
+
+        location /.well-known/acme-challenge/ {
+                alias $web_dir/certificate/challenges/;
+                try_files \$uri =404;
+        }
+        location /download {
+                autoindex on;
+                autoindex_exact_size off;
+                autoindex_localtime on;
+        }
+}
+EOF
+cat > $web_dir/index.html << EOF
+generate https website succssfully<br/>
+this is the index.html of $web_first_domain <br/>
+yout can visit this page from $web_domains
+EOF
+# current_user=$USER
+# current_user=$(id -un) not work for sudo
+current_user=$(who am i|awk '{print $1}')
+current_user_group=$(id -gn $current_user)
+chown -R $current_user:$current_user_group $web_dir
+chown $current_user:$current_user_group $nginx_config_dir"/conf.d/"$nginx_web_config_file
+chmod -R 755 $web_dir
+service nginx restart
+cat << EOF
+generate https website succssfully
+your website directory is $web_dir
+you can visit your website through
+EOF
+for web_domain in ${web_domains[@]}
+do
+    echo https://$web_domain
+done
+echo 'press any key to quit'
+read -p '> '
+exit 0
