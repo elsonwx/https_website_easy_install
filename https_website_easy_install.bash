b124d24cd5cb1ee785f07073157a60a97c4a38ba
elsonwx <elsonwx@gmail.com>
2017-06-15 12:07:23 +0800

    update script

--- https_website_easy_install.bash
+++ https_website_easy_install.bash
@@ -146,9 +146,9 @@ wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > inter
 cat /tmp/signed.crt intermediate.pem > $web_dir/certificate/chained.pem
 service nginx reload
 EOF
-# (crontab -u $current_user -l ; echo "1 1 1 * * bash $web_dir/certificate/renew_cert.bash >/dev/null 2 >> /var/log/renew_cert_error.log") | crontab -u $current_user -
+# (crontab -u $current_user -l ; echo "1 1 1 * * bash $web_dir/certificate/renew_cert.bash >> /var/log/renew_cert_error.log 2 >> /var/log/renew_cert.log") | crontab -u $current_user -
 # nginx reload need root privilege,so the renew task need to be added in root's crontab
-(crontab -l; echo "1 1 1 * * bash $web_dir/certificate/renew_cert.bash >/dev/null 2 >> /var/log/renew_cert_error.log") | crontab -
+(crontab -l; echo "1 1 1 * * bash $web_dir/certificate/renew_cert.bash >> /var/log/renew_cert_error.log 2 >> /var/log/renew_cert.log") | crontab -
 echo "create renewal certificate task succ!"
 read -p 'press any key to quit'
 exit 0
